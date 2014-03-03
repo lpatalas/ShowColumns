df273a40afe9bf14b0e5b70c67c5ace95e0da321
Łukasz Patalas <luke.patalas@gmail.com>
2014-03-03 20:40:10 +0100

    Remove blank lines before and after listing

--- Format-Columns.ps1
+++ Format-Columns.ps1
@@ -209,11 +209,8 @@ function Format-Columns {
                 Show-GroupedItems $items $itemWidths
             }
             else {
-                Write-Host
                 Write-Columns $items $itemWidths 1
             }
-            
-            Write-Host
         }
     }
 }
