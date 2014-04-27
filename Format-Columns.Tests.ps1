58530c994bf316b293edba00b8fd2ad507d0e757
Łukasz Patalas <luke.patalas@gmail.com>
2014-04-27 14:03:43 +0200

    Add test for multiple columns and rows

--- Format-Columns.Tests.ps1
+++ Format-Columns.Tests.ps1
@@ -15,8 +15,8 @@ function Create-File($path) {
     New-Item $path -ItemType File | Out-Null
 }
 
-function Generate-Files($directoryPath, $count = 3) {
-    1..$count | %{ Create-File (Join-Path $directoryPath "$_.txt") }
+function Generate-Files($directoryPath, $count = 3, $prefix = '') {
+    1..$count | %{ Create-File (Join-Path $directoryPath "$prefix$_.txt") }
 }
 
 function Prepare-TestData {
@@ -26,13 +26,16 @@ function Prepare-TestData {
 
     Clear-Directory $TestsDir
     Create-Directory "$TestsDir\Empty"
+
     Create-Directory "$TestsDir\Subfolders"
     Generate-Files "$TestsDir\Subfolders"
-
     foreach ($folderNumber in 1..3) {
         Create-Directory "$TestsDir\Subfolders\$folderNumber"
         Generate-Files "$TestsDir\Subfolders\$folderNumber"
     }
+
+    Create-Directory "$TestsDir\Columns"
+    Generate-Files "$TestsDir\Columns" -count 100 -prefix test
 }
 
 function Test($description, $script) {
@@ -50,6 +53,10 @@ Test 'empty directory' {
 
 Test 'listing folder contents non-recursively' {
     Get-ChildItem "$TestsDir\Subfolders" | Format-Columns
+}
+
+Test 'organizing items into multiple columns' {
+    Get-ChildItem "$TestsDir\Columns" | Format-Columns
 }
 
 Test 'listing folder contents non-recursively in reverse order' {
