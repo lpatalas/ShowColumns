f609cbc5464a688f46eaa00fe0c964c2a3a1b472
Lukasz Patalas <lpatalas@gmail.com>
2018-11-17 11:09:18 +0100

    Add C# project

---
+++ src/FormatColumnsCmdlet.cs
@@ -0,0 +1,16 @@
+﻿using System.Management.Automation;
+
+namespace FormatColumns
+{
+	[Cmdlet("Format", "Columns")]
+	public class FormatColumnsCmdlet : Cmdlet
+	{
+		[Parameter(ValueFromPipeline = true)]
+		public PSObject InputObject { get; set; }
+
+		protected override void BeginProcessing()
+		{
+			WriteObject("Test");
+		}
+	}
+}
