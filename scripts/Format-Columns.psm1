fbff543236e7c44d7358e11338ba20c0921f9916
Lukasz Patalas <lpatalas@gmail.com>
2018-11-18 21:14:40 +0100

    Remove old scripts

--- scripts/Format-Columns.psm1
+++
@@ -1,7 +0,0 @@
-$ModulePath = (Split-Path $MyInvocation.MyCommand.Definition)
-
-. "$ModulePath\Format-Columns.ps1"
-. "$ModulePath\Get-ChildItemColumns.ps1"
-
-Export-ModuleMember -Function Format-Columns
-Export-ModuleMember -Function Get-ChildItemColumns
