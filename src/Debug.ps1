3937e07b000b77588b9925a898d816a31c5fa61d
Lukasz Patalas <lpatalas@gmail.com>
2018-11-18 12:18:57 +0100

    Implement `Property` parameter supporting both `string` and `ScriptBlock`

--- src/Debug.ps1
+++ src/Debug.ps1
@@ -1,1 +1,13 @@
 ﻿Import-Module "$PSScriptRoot\FormatColumns.dll"
+
+$objs = @(
+	@{ Name = 'First' }
+	@{ Name = 'Second' }
+	@{ Name = 'Third' }
+)
+
+Write-Host "By property name:" -ForegroundColor Yellow
+$objs | Format-Columns -Property { $_.Name }
+
+Write-Host "By script block:" -ForegroundColor Yellow
+$objs | Format-Columns -Property Name
--- src/FormatColumnsCmdlet.cs
+++ src/FormatColumnsCmdlet.cs
@@ -1,16 +1,54 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 
 namespace FormatColumns
 {
 	[Cmdlet("Format", "Columns")]
 	public class FormatColumnsCmdlet : Cmdlet
 	{
+		private Func<PSObject, string> propertyAccessor;
+
 		[Parameter(ValueFromPipeline = true)]
 		public PSObject InputObject { get; set; }
 
-		protected override void BeginProcessing()
+		[Parameter]
+		public object Property { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (Property is string propertyName)
+                propertyAccessor = CreateByNamePropertyAccessor(propertyName);
+            else if (Property is ScriptBlock scriptBlock)
+                propertyAccessor = CreateScriptBlockPropertyAccessor(scriptBlock);
+            else
+                throw new ApplicationException(
+                    $"{nameof(Property)} parameter must be a String or ScriptBlock"
+                    + $" but found '{Property?.GetType()?.FullName ?? "<null>"}'.");
+        }
+
+        protected override void ProcessRecord()
 		{
-			WriteObject("Test");
+            var value = propertyAccessor(InputObject);
+			WriteObject($"Script: {value}");
 		}
+
+        private Func<PSObject, string> CreateByNamePropertyAccessor(string propertyName)
+            => obj
+                => obj.BaseObject is IDictionary dictionary
+                    ? dictionary[propertyName]?.ToString()
+                    : obj.Properties[propertyName]?.ToString();
+
+        private Func<PSObject, string> CreateScriptBlockPropertyAccessor(ScriptBlock scriptBlock)
+            => obj
+                => scriptBlock.InvokeWithContext(
+                    functionsToDefine: null,
+                    variablesToDefine: new List<PSVariable>(1) { new PSVariable("_", obj) })
+                    ?.FirstOrDefault()
+                    ?.ToString();
+
 	}
 }
