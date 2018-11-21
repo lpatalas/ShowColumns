39b35d1eb89b9a6998d9c897c036753ace9f0b5d
Lukasz Patalas <lpatalas@gmail.com>
2018-11-21 07:54:18 +0100

    Implement `ToString()` method for version returned from `ConvertFrom-VersionString`

--- build/ConvertFrom-VersionString.ps1
+++ build/ConvertFrom-VersionString.ps1
@@ -15,8 +15,40 @@ if ($prereleaseLabel) {
     $prerelaseNumber = [Int32]::Parse($matches['number'].TrimStart('0'))
 }
 
-return [PSCustomObject]@{
+$isMatch = $baseVersion -match '(\d+)\.(\d+)\.(\d+)'
+if (-not $isMatch) {
+    throw "Can't parse minor.major.patch from string '$baseVersion'"
+}
+
+$major = [Int32]::Parse($matches[1])
+$minor = [Int32]::Parse($matches[2])
+$patch = [Int32]::Parse($matches[3])
+$comparableBaseVersion = $major.ToString('000') + '.' + $minor.ToString('000') + '.' + $patch.ToString('000')
+
+$version = [PSCustomObject]@{
     BaseVersion = $baseVersion
     PrereleaseLabel = $prereleaseLabel
     PrereleaseNumber = $prerelaseNumber
+    Major = $major
+    Minor = $minor
+    Patch = $patch
+    ComparableBaseVersion = $comparableBaseVersion
 }
+
+$version | Add-Member `
+    -MemberType ScriptMethod `
+    -Name ToString `
+    -Force `
+    -Value {
+        if ($this.PrereleaseLabel) {
+            $v = $this.BaseVersion
+            $pre = $this.PrereleaseLabel
+            $n = $this.PrereleaseNumber.ToString('000')
+            return "$v-$pre$n"
+        }
+        else {
+            return $this.BaseVersion
+        }
+    }
+
+return $version
